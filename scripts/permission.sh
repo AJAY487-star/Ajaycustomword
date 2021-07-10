bbe7c0c0a99f280fa9a817ae2bf47c4b5bb2b543
root <root@ip-172-31-2-20.ap-south-1.compute.internal>
2021-07-10 08:21:58 +0000

    scripts

---
+++ scripts/permission.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+chmod -R 755 /var/www/html/wordpress.squareops.xyz/public_html/wp-content
+chown -R www-data:www-data /var/www/html/wordpress.squareops.xyz/public_html/wp-content
+chmod -R 755 /var/www/html/var/www/html/wordpress.squareops.xyz/public_html/wp-content/uploads
+chown -R www-data:www-data /var/www/html/wordpress.squareops.xyz/public_html/wp-content/uploads
